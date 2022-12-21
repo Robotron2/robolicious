3c83bf3958dc0a831e3897504ba980e618f2225d
Theophilus <theophilusadesola002@gmail.com>
2022-12-21 18:19:10 +0100

    File structure/ blueprint set

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Popular from "../components/Popular"
+import Veggie from "../components/Veggie"
+
+const Home = () => {
+	return (
+		<div>
+			<Popular />
+			<Veggie />
+		</div>
+	)
+}
+
+export default Home
---
+++ src/pages/Pages.js
@@ -0,0 +1,11 @@
+import Home from "./Home"
+
+const Pages = () => {
+	return (
+		<div>
+			<Home />
+		</div>
+	)
+}
+
+export default Pages
