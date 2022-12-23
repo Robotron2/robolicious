460b59d5cbdc7de21208ecce4818e666a741e2ee
Theophilus <theophilusadesola002@gmail.com>
2022-12-23 22:56:46 +0100

    useEffect ensures the fetch request is done once

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,17 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Popular = () => {
+	const [popular, setPopular] = useState([])
+
 	const getpopular = async () => {
 		const url = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
 		const urlData = await url.json()
 		console.log(urlData)
+		setPopular(urlData.recipes)
 	}
 
 	useEffect(() => {
 		getpopular()
 	}, [])
 
-	return <div>Popular</div>
+	return (
+		<div>
+			Popular
+			{popular.map((recipe) => {
+				return (
+					<div key={recipe.id}>
+						<p>{recipe.title + " title"}</p>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Popular
