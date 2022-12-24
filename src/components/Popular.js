9411fb4124aa28b441df9a41c4561c9859f3308e
Theophilus <theophilusadesola002@gmail.com>
2022-12-24 14:23:35 +0100

    Random popular picks can now be rendered

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
+import styled from "styled-components"
 
 const Popular = () => {
 	const [popular, setPopular] = useState([])
 
 	const getpopular = async () => {
-		const url = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
+		const url = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=2`)
 		const urlData = await url.json()
 		console.log(urlData)
 		setPopular(urlData.recipes)
@@ -16,16 +17,29 @@ const Popular = () => {
 
 	return (
 		<div>
-			Popular
 			{popular.map((recipe) => {
 				return (
-					<div key={recipe.id}>
-						<p>{recipe.title + " title"}</p>
-					</div>
+					<Wrapper key={recipe.id}>
+						<h3>Popular Picks</h3>
+
+						<Card>
+							<p>{recipe.title}</p>
+							<img src={recipe.image} alt={recipe.title} />
+						</Card>
+					</Wrapper>
 				)
 			})}
 		</div>
 	)
 }
+
+const Wrapper = styled.div`
+	margin: 4rem 0rem;
+`
+
+const Card = styled.div`
+	min-height: 25rem;
+	border-radius: 2rem;
+`
 
 export default Popular
