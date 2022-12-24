b99b56c07c92315aefba92e581279bc6d9c1ae52
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-24 20:26:30 +0100

    search input can now be used to query for particular recipe

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,6 +9,7 @@ const Search = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
+		navigate(`searched/${input}`)
 	}
 
 	return (
--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,5 +1,22 @@
+import { useEffect } from "react"
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+
 const SearchedPage = () => {
-	return <div>Searched Paage</div>
+	const [searchedRecipes, setSearchedRecipes] = useState([])
+	const params = useParams()
+
+	const getSearchData = async (name) => {
+		const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+		const recipes = await data.json()
+		setSearchedRecipes(recipes.results)
+	}
+
+	useEffect(() => {
+		getSearchData(params.searchInput)
+	}, [params.searchInput])
+
+	return <div></div>
 }
 
 export default SearchedPage
