b7ff71c77380d00b0b0c4188eb2856574620fe2e
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-24 21:21:07 +0100

    activeTab states added for Instructions and Ingredient buttons

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,10 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
 
 const Recipe = () => {
 	const [details, setDetails] = useState({})
+	const [activeTab, setActiveTab] = useState("instructions")
 	const params = useParams()
 
 	const fetchDetails = async () => {
@@ -17,7 +18,58 @@ const Recipe = () => {
 		fetchDetails()
 	}, [params.recipeId])
 
-	return <div>{details.title}</div>
+	return (
+		<DetailWrapper>
+			<div>
+				<h2>{details.title}</h2>
+				<img src={details.image} alt="" />
+			</div>
+			<Info>
+				<SButton className={activeTab === "instructions" ? "active" : ""} onClick={() => setActiveTab("instructions")}>
+					Instructions
+				</SButton>
+				<SButton className={activeTab === "ingredients" ? "active" : ""} onClick={() => setActiveTab("ingredients")}>
+					Ingredients
+				</SButton>
+			</Info>
+		</DetailWrapper>
+	)
 }
+
+const DetailWrapper = styled.div`
+	margin-top: 10rem;
+	margin-bottom: 5rem;
+	display: flex;
+	.active {
+		background: linear-gradient(35deg, #494949, #313131);
+		color: white;
+	}
+
+	h2 {
+		margin-bottom: 2rem;
+	}
+
+	li {
+		font-size: 1.2rem;
+		line-height: 2.5rem;
+	}
+
+	ul {
+		margin: 2 0 0 0;
+	}
+`
+
+const SButton = styled.button`
+	padding: 1rem 2rem;
+	color: #313131;
+	background: #ffff;
+	border: 2px solid black;
+	margin-right: 2rem;
+	font-weight: 600;
+`
+
+const Info = styled.div`
+	margin-left: 10rem;
+`
 
 export default Recipe
