8ba75b25536fb72c135149762f0395f89ab1bb3f
Theophilus <theophilusadesola002@gmail.com>
2022-12-24 14:47:21 +0100

    Styled-div Card got styled and Styled-div Gradient added

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -22,13 +22,22 @@ const Popular = () => {
 			<Wrapper>
 				<h3>Popular Picks</h3>
 
-				<Splide>
+				<Splide
+					options={{
+						perPage: 4,
+						arrows: false,
+						pagination: false,
+						drag: "free",
+						gap: "5rem"
+					}}
+				>
 					{popular.map((recipe) => {
 						return (
 							<SplideSlide key={recipe.id}>
 								<Card>
 									<p>{recipe.title}</p>
 									<img src={recipe.image} alt={recipe.title} />
+									<Gradient />
 								</Card>
 							</SplideSlide>
 						)
@@ -47,10 +56,41 @@ const Card = styled.div`
 	min-height: 25rem;
 	border-radius: 2rem;
 	overflow: hidden;
+	position: relative;
 
 	img {
 		border-radius: 2rem;
+		position: absolute;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
 	}
+
+	p {
+		position: absolute;
+		z-index: 10;
+		left: 50%;
+		bottom: 0%;
+		transform: translate(-50%, 0%);
+		color: white;
+		width: 100%;
+		text-align: center;
+		font-weight: 600;
+		font-size: 1rem;
+		height: 40%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+`
+
+const Gradient = styled.div`
+	position: absolute;
+	z-index: 3;
+	width: 100%;
+	height: 100%;
+	background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `
 
 export default Popular
