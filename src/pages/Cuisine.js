49b207e108603c51db392f5851268d7edf8834bd
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-24 16:47:53 +0100

    Complex search begins

--- src/pages/Cuisine.js
+++ src/pages/Cuisine.js
@@ -1,5 +1,24 @@
+import styled from "styled-components"
+import { motion } from "framer-motion"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 const Cuisine = () => {
-	return <div>Cuisine</div>
+	const [cuisine, setCuisine] = useState([])
+	let params = useParams()
+
+	const getCuisine = async (name) => {
+		const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+		const recipes = await data.json()
+		setCuisine(recipes.results)
+	}
+
+	useEffect(() => {
+		getCuisine(params.type)
+		console.log(params.type)
+	}, [params.type])
+
+	return <div></div>
 }
 
 export default Cuisine
