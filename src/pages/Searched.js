fe42e6d04b9e7b2cca2e612859d1d4c9b9869258
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-24 21:56:55 +0100

    Ingredients and details are now shown independently

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import styled from "styled-components"
 
 const SearchedPage = () => {
@@ -21,8 +21,10 @@ const SearchedPage = () => {
 			{searchedRecipes.map((item) => {
 				return (
 					<Card key={item.id}>
-						<img src={item.image} alt={item.title} />
-						<h4>{item.title}</h4>
+						<Link to={`recipe/${item.id}`}>
+							<img src={item.image} alt={item.title} />
+							<h4>{item.title}</h4>
+						</Link>
 					</Card>
 				)
 			})}
