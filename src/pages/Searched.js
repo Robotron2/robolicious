f3e9fd623a8f57e586e3792158a24d26172bfced
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-24 20:32:48 +0100

    Searched data can now be seen in the DOM

--- src/pages/Searched.js
+++ src/pages/Searched.js
@@ -1,13 +1,13 @@
-import { useEffect } from "react"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import styled from "styled-components"
 
 const SearchedPage = () => {
 	const [searchedRecipes, setSearchedRecipes] = useState([])
-	const params = useParams()
+	let params = useParams()
 
 	const getSearchData = async (name) => {
-		const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+		const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`)
 		const recipes = await data.json()
 		setSearchedRecipes(recipes.results)
 	}
@@ -16,7 +16,34 @@ const SearchedPage = () => {
 		getSearchData(params.searchInput)
 	}, [params.searchInput])
 
-	return <div></div>
+	return (
+		<div className="grid-class">
+			{searchedRecipes.map((item) => {
+				return (
+					<Card key={item.id}>
+						<img src={item.image} alt={item.title} />
+						<h4>{item.title}</h4>
+					</Card>
+				)
+			})}
+		</div>
+	)
 }
+
+const Card = styled.div`
+	justify-content: center;
+	align-item: center;
+	img {
+		width: 100%;
+		border-radius: 2rem;
+	}
+	a {
+		text-decoration: none;
+	}
+	h4 {
+		text-align: center;
+		padding: 1rem;
+	}
+`
 
 export default SearchedPage
