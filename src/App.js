6ac2c08a8254b66a7d6d8b60a2efefafcdd01f4e
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-26 14:58:06 +0100

    search field added and styled

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 		<div className="App">
 			<BrowserRouter>
 				<Header />
-				{/* <Search /> */}
+				<Search />
 				<Category />
 				<Pages />
 			</BrowserRouter>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
-import { FaSearch } from "react-icons/fa"
+
 import { useNavigate } from "react-router-dom"
-import styled from "styled-components"
 
 const Search = () => {
 	const [input, setInput] = useState("")
@@ -13,41 +12,14 @@ const Search = () => {
 	}
 
 	return (
-		<div>
-			<FormStyle className="search-form" onSubmit={handleSubmit}>
-				<div>
-					<FaSearch />
-					<input type="text" onChange={(e) => setInput(e.target.value)} value={input} />
+		<form onSubmit={handleSubmit}>
+			<div class="container">
+				<div class="inputs">
+					<input type="text" onChange={(e) => setInput(e.target.value)} value={input} placeholder="Search Recipe" />
 				</div>
-			</FormStyle>
-		</div>
+			</div>
+		</form>
 	)
 }
-
-const FormStyle = styled.form`
-	margin: 0 2rem;
-	position: relative;
-	width: 100%;
-
-	input {
-		border: none;
-		background: linear-gradient(35deg, #494949, #313131);
-		font-size: 1.5rem;
-		color: white;
-		padding: 1rem 3rem;
-		border: none;
-		border-radius: 1rem;
-		outline: none;
-		width: 100%;
-	}
-
-	svg {
-		position: absolute;
-		top: 50%;
-		left: 0%;
-		transform: translate(100%, -50%);
-		color: white;
-	}
-`
 
 export default Search
