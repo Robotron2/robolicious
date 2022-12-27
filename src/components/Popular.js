2dcfcf9801872b716be850c043b277506cd9385a
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-27 19:58:06 +0100

    Popular Picks section styled

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -34,41 +34,44 @@ const Popular = () => {
 	}, [])
 
 	return (
-		<div className="container py-4 px-4 justify-content-center bg-danger">
-			<Swiper
-				freeMode={true}
-				grabCursor={true}
-				modules={[FreeMode]}
-				className="mySwiper"
-				slidesPerView={5}
-				spaceBetween={10}
-				breakpoints={{
-					960: {
-						slidesPerView: 4,
-						spaceBetween: 8
-					},
-					720: {
-						slidesPerView: 3,
-						spaceBetween: 6
-					},
-					540: {
-						slidesPerView: 2,
-						spaceBetween: 4
-					},
-					220: {
-						slidesPerView: 1,
-						spaceBetween: 2
-					}
-				}}
-			>
-				{popular.map((recipe) => {
-					return (
-						<SwiperSlide>
-							<CardComponent image={recipe.image} title={recipe.title} />
-						</SwiperSlide>
-					)
-				})}
-			</Swiper>
+		<div>
+			<h3 className="text-center mb-2">Popular Picks</h3>
+			<div className="container py-4 px-4 justify-content-center myPopular">
+				<Swiper
+					freeMode={true}
+					grabCursor={true}
+					modules={[FreeMode]}
+					className="mySwiper"
+					slidesPerView={5}
+					spaceBetween={10}
+					breakpoints={{
+						960: {
+							slidesPerView: 4,
+							spaceBetween: 8
+						},
+						720: {
+							slidesPerView: 3,
+							spaceBetween: 6
+						},
+						540: {
+							slidesPerView: 2,
+							spaceBetween: 4
+						},
+						220: {
+							slidesPerView: 1,
+							spaceBetween: 2
+						}
+					}}
+				>
+					{popular.map((recipe) => {
+						return (
+							<SwiperSlide>
+								<CardComponent image={recipe.image} title={recipe.title} />
+							</SwiperSlide>
+						)
+					})}
+				</Swiper>
+			</div>
 		</div>
 	)
 }
