163be15bcd348139fb33f60e72fd42f209d79997
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-29 07:59:48 +0100

    Loading state svg added to recipe page

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -6,14 +6,14 @@ import { motion } from "framer-motion"
 const Recipe = () => {
 	const [details, setDetails] = useState({})
 	const [activeTab, setActiveTab] = useState("instructions")
-	const [isLoading, setIsLoading] = useState(false)
+	const [isLoading, setIsLoading] = useState(true)
 	const params = useParams()
 
 	const fetchDetails = async () => {
 		const data = await fetch(`https://api.spoonacular.com/recipes/${params.recipeId}/information?apiKey=${process.env.REACT_APP_API_KEY}`)
 		const detailData = await data.json()
 		setDetails(detailData)
-		setIsLoading(true)
+		setIsLoading(false)
 	}
 
 	useEffect(() => {
@@ -21,37 +21,56 @@ const Recipe = () => {
 	}, [params.recipeId])
 
 	return (
-		<motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} className="container mt-5 ">
-			<div className="recipe-container">
-				<div className="container">
-					<h3>{details.title}</h3>
-					<img src={details.image} alt="" className="mb-3" />
-					<div className="container btn-container">
-						<button className={activeTab === "instructions" ? "active btn btn-dark recipe-btn m-4" : " btn btn-outline-dark m-4"} onClick={() => setActiveTab("instructions")}>
-							Instructions
-						</button>
-						<button className={activeTab === "ingredients" ? "active  btn btn-danger recipe-btn m-4" : " btn btn-outline-danger m-4"} onClick={() => setActiveTab("ingredients")}>
-							Ingredients
-						</button>
-					</div>
-				</div>
-				<div className="container pt-5 body">
-					{activeTab === "instructions" && (
-						<div>
-							<p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
-							<p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
+		<div className="container">
+			{isLoading && (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					style={{ margin: "auto", background: "inherit", display: "block", shapeRendering: "auto" }}
+					width="200px"
+					height="200px"
+					viewBox="0 0 100 100"
+					preserveAspectRatio="xMidYMid"
+				>
+					<circle cx="50" cy="50" r="32" strokeWidth="1" stroke="#fe718d" strokeDasharray="50.26548245743669 50.26548245743669" fill="none" strokeLinecap="round">
+						<animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+					</circle>
+				</svg>
+			)}
+
+			{!isLoading && (
+				<motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }} className="container mt-5 ">
+					<div className="recipe-container">
+						<div className="container">
+							<h3>{details.title}</h3>
+							<img src={details.image} alt="" className="mb-3" />
+							<div className="container btn-container">
+								<button className={activeTab === "instructions" ? "active btn btn-dark recipe-btn m-4" : " btn btn-outline-dark m-4"} onClick={() => setActiveTab("instructions")}>
+									Instructions
+								</button>
+								<button className={activeTab === "ingredients" ? "active  btn btn-danger recipe-btn m-4" : " btn btn-outline-danger m-4"} onClick={() => setActiveTab("ingredients")}>
+									Ingredients
+								</button>
+							</div>
 						</div>
-					)}
-					{activeTab === "ingredients" && (
-						<ul className="pt-5">
-							{details.extendedIngredients.map((ingredient) => {
-								return <li key={ingredient.id}>{ingredient.original}</li>
-							})}
-						</ul>
-					)}
-				</div>
-			</div>
-		</motion.div>
+						<div className="container pt-5 body">
+							{activeTab === "instructions" && (
+								<div>
+									<p dangerouslySetInnerHTML={{ __html: details.summary }}></p>
+									<p dangerouslySetInnerHTML={{ __html: details.instructions }}></p>
+								</div>
+							)}
+							{activeTab === "ingredients" && (
+								<ul className="pt-5">
+									{details.extendedIngredients.map((ingredient) => {
+										return <li key={ingredient.id}>{ingredient.original}</li>
+									})}
+								</ul>
+							)}
+						</div>
+					</div>
+				</motion.div>
+			)}
+		</div>
 	)
 }
 
