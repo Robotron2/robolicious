41e9d178d9dd033811af1e47d32aa71b3b7d5252
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-30 09:09:41 +0100

    Footer component styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,31 @@
 import { Link } from "react-router-dom"
+import { BsGithub, BsTwitter } from "react-icons/bs"
+import { SiGmail } from "react-icons/si"
 
 const Footer = () => {
+	let currentYear = new Date().getFullYear()
+
 	return (
-		<div class="container">
-			<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+		<div class="container mt-5 pt-3">
+			<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top">
 				<div class="col-md-4 d-flex align-items-center">
-					<span class="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
+					<span class="mb-3 mb-md-0 text-muted">&copy; {currentYear} Robotron</span>
 				</div>
 
 				<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
 					<li class="ms-3">
-						<Link>
-							<svg class="bi" width="24" height="24">
-								<use xlink:href="#twitter" />
-							</svg>
+						<Link to={"/contact"}>
+							<SiGmail className="two" />
 						</Link>
 					</li>
 					<li class="ms-3">
-						<Link>
-							<svg class="bi" width="24" height="24">
-								<use xlink:href="#twitter" />
-							</svg>
+						<Link to={"/https://github.com/Robotron2"}>
+							<BsGithub className="two" />
 						</Link>
 					</li>
 					<li class="ms-3">
-						<Link>
-							<svg class="bi" width="24" height="24">
-								<use xlink:href="#twitter" />
-							</svg>
+						<Link to={"/https://twitter.com/Robotron090343"}>
+							<BsTwitter className="two" />
 						</Link>
 					</li>
 				</ul>
