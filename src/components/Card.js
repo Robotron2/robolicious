33f5eb28b28cf32ff8e603d3e0afa742f1e533a5
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-30 11:08:02 +0100

    Styles improved

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,11 @@ const CardComponent = (props) => {
 	return (
 		<div>
 			<Card className="p-0 overflow-hidden h-100 shadow myCard">
-				<div className="overflow-hidden rounded p-0 bg-light">
+				<div className="overflow-hidden rounded p-0 ">
 					<Card.Img variant="top" src={imgSrc} />
 				</div>
 				<Card.Body className="text-center ">
-					<Card.Title className="display-6">{title}</Card.Title>
+					<Card.Title className="display-6 text-danger">{title}</Card.Title>
 				</Card.Body>
 			</Card>
 		</div>
