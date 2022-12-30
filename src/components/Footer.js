280f9e0c6d3bd614d2f5e53993e051edbe54826f
Theophilus Adeleke <theophilusadesola002@gmail.com>
2022-12-30 10:18:15 +0100

    All react strict mode and Eslint errors fixed.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,24 @@ const Footer = () => {
 	let currentYear = new Date().getFullYear()
 
 	return (
-		<div class="container mt-5 pt-3">
-			<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top">
-				<div class="col-md-4 d-flex align-items-center">
-					<span class="mb-3 mb-md-0 text-muted">&copy; {currentYear} Robotron</span>
+		<div className="container mt-5 pt-3">
+			<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-5 border-top">
+				<div className="col-md-4 d-flex align-items-center">
+					<span className="mb-3 mb-md-0 text-muted">&copy; {currentYear} Robotron</span>
 				</div>
 
-				<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-					<li class="ms-3">
+				<ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+					<li className="ms-3">
 						<Link to={"/contact"}>
 							<SiGmail className="two" />
 						</Link>
 					</li>
-					<li class="ms-3">
+					<li className="ms-3">
 						<a href="https://github.com/Robotron2" target="_blank" rel="noopener noreferrer">
 							<BsGithub className="two" />
 						</a>
 					</li>
-					<li class="ms-3">
+					<li className="ms-3">
 						<a href="https://twitter.com/Robotron090343" target="_blank" rel="noreferrer">
 							<BsTwitter className="two" />
 						</a>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -22,29 +22,29 @@ const Contact = () => {
 				</motion.div>
 			</header>
 
-			<section id="contact" class="contact">
-				<div class="container" data-aos="fade-up">
-					<div class="section-header m-3 text-center">
+			<section id="contact" className="contact">
+				<div className="container" data-aos="fade-up">
+					<div className="section-header m-3 text-center">
 						<h3>
 							Need my service? <span>Contact Me</span>
 						</h3>
 					</div>
 
-					<form action="https://formspree.io/f/mvonpjlv" method="POST" class="emailForm p-3 p-md-4">
-						<div class="row">
-							<div class="col-xl-6 form-group">
-								<input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required />
+					<form action="https://formspree.io/f/mvonpjlv" method="POST" className="emailForm p-3 p-md-4">
+						<div className="row">
+							<div className="col-xl-6 form-group">
+								<input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
 							</div>
-							<div class="col-xl-6 form-group">
-								<input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required />
+							<div className="col-xl-6 form-group">
+								<input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
 							</div>
 						</div>
-						<div class="form-group">
-							<textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+						<div className="form-group">
+							<textarea className="form-control" name="message" rows="5" placeholder="Message" required></textarea>
 						</div>
 
-						<div class="text-center">
-							<button class="btn btn-outline-danger rounded-pill btn-md">Send Message</button>
+						<div className="text-center">
+							<button className="btn btn-outline-danger rounded-pill btn-md">Send Message</button>
 						</div>
 					</form>
 				</div>
